4a341af75abf4d83cf0f4ce395e0c9e4ea338337
BrownSim <iddadidkfq77594>
2017-11-21 16:03:28 +0100

    Update theme

---
+++ Resources/public/js/menu.js
@@ -0,0 +1,66 @@
+jQuery(function ($) {
+    var CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
+        $BODY = $('body'),
+        $MENU_TOGGLE = $('#menu_toggle'),
+        $SIDEBAR_MENU = $('#sidebar-menu');
+
+    $SIDEBAR_MENU.find('a').on('click', function (ev) {
+        var $li = $(this).parent();
+
+        if ($li.is('.active')) {
+            $li.removeClass('active active-sm');
+            $('ul:first', $li).slideUp(function () {
+            });
+        } else {
+            // prevent closing menu if we are on child menu
+            if (!$li.parent().is('.child_menu')) {
+                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                $SIDEBAR_MENU.find('li ul').slideUp();
+            } else {
+                if ($BODY.is(".nav-sm")) {
+                    $SIDEBAR_MENU.find("li").removeClass("active active-sm");
+                    $SIDEBAR_MENU.find("li ul").slideUp();
+                }
+            }
+            $li.addClass('active');
+
+            $('ul:first', $li).slideDown();
+
+        }
+    });
+
+// toggle small or large menu
+    $MENU_TOGGLE.on('click', function () {
+        console.log('clicked - menu toggle');
+
+        if ($BODY.hasClass('nav-md')) {
+            $SIDEBAR_MENU.find('li.active ul').hide();
+            $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+        } else {
+            $SIDEBAR_MENU.find('li.active-sm ul').show();
+            $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+        }
+
+        $BODY.toggleClass('nav-md nav-sm');
+
+        $('.dataTable').each(function () {
+            $(this).dataTable().fnDraw();
+        });
+    });
+
+    // check active menu
+    $SIDEBAR_MENU.find("a").each(function () {
+        if ($(this).attr("href").slice(0, -5) !== '' && CURRENT_URL.indexOf($(this).attr("href").slice(0, -5)) >= 0) {
+            $(this).parent('li').addClass('current-page').parents('ul').slideDown().parent().addClass('active');
+        }
+    });
+
+    // fixed sidebar
+    if ($.fn.mCustomScrollbar) {
+        $('.menu_fixed').mCustomScrollbar({
+            autoHideScrollbar: true,
+            theme: 'minimal',
+            mouseWheel: {preventDefault: true}
+        });
+    }
+});
